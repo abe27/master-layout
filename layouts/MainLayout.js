fdeeaee237f30ca60465729de99acf704da5abea
Taweechai Yuenyang <krumii.it@gmail.com>
2022-12-23 14:15:03 +0700

    create layout

---
+++ layouts/MainLayout.js
@@ -0,0 +1,20 @@
+import { NavBar, BreadcrumbBar,Header } from "../components";
+
+const MainLayout = ({ title, description, children }) => {
+  return (
+    <div className="min-h-full">
+      <Header title={title} description={description}/>
+      <NavBar />
+      <BreadcrumbBar />
+      <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          {/* Replace with your content */}
+          <div className="sm:px-0">{children}</div>
+          {/* /End replace */}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default MainLayout;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,9 @@
-import { NavBar, BreadcrumbBar } from "../components";
+import { MainLayout } from "../layouts";
 const IndexPage = () => {
   return (
-    <>
-      <div className="min-h-full">
-        <NavBar/>
-        <BreadcrumbBar />
-        <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            {/* Replace with your content */}
-            <div className="sm:px-0">
-              <h1>Hello</h1>
-            </div>
-            {/* /End replace */}
-          </div>
-        </main>
-      </div>
-    </>
+    <MainLayout title={'Test LayOut'} description={'Test Description'}>
+      <h1>Hello</h1>
+    </MainLayout>
   );
 };
 
